0c4bf868dfbc07d85e079e2d4392f0e2bd8a2d48
herringvoices <nalherring@gmail.com>
2025-03-13 16:09:13 -0500

    fix: update search functionality and styling in AddFriends and MyFriends components

--- client/src/components/friendsComponents/MyFriends.jsx
+++ client/src/components/friendsComponents/MyFriends.jsx
@@ -28,19 +28,17 @@ function MyFriends() {
     <>
       <div className="flex justify-center px-2 py-2">
         <input
-          className="!px-2 py-1 w-2/3 mx-2 text-dark rounded-md"
+          className="block me-2 p-2 w-full bg-dark border border-primary/50 text-primary rounded-md focus:border-primary focus:outline-none"
           type="text"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           onKeyDown={handleKeyPress}
           placeholder="Search a friend's name or username..."
         />
-        <button
-          className="bg-primary text-dark rounded-md"
-          onClick={handleSearch}
-        >
+        <button className="" onClick={handleSearch}>
           <FontAwesomeIcon
-            className="p-2 mx-auto my-auto"
+            className="p-2 mx-auto bg-primary hover:bg-primary/80 text-dark  rounded-md my-auto"
+            size="lg"
             icon="fa-solid fa-magnifying-glass"
           />
         </button>
